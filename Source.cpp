594ee1110fa431fa84139c9fb1b81190194b174a
duy.tran-khanh@banvien.com.vn <duy.tran-khanh@banvien.com>
2022-03-11 10:36:40 +0700

    Commit Thien's source

---
+++ Source.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdlib.h>
+#include <windows.h>
+#define MAX 20
+
+using namespace std;
+
+struct student
+{
+	char MSSV[10], hoten[30];
+	int gioitinh;
+	char diachi[50];
+	float dtb;
+};
+
+int Nhap1SV(student list[], int n)
+{
+	system("cls");
+	cout << "Nhap MSSV:";
+	cin >> list[n].MSSV;
+	std::cin.ignore(32767, '\n');
+	cout << "Nhap ten:";
+	fgets(list[n].hoten, sizeof(list[n].hoten), stdin);
+	do
+	{
+		cout << "Nhap gioi tinh (1: Nu; 0: Nam):";
+		cin >> list[n].gioitinh;
+	} while (list[n].gioitinh != 0 && list[n].gioitinh != 1);
+	std::cin.ignore(32767, '\n');
+	cout << "Nhap dia chi:";
+	fgets(list[n].diachi, sizeof(list[n].diachi), stdin);
+	cout << "Nhap diem TB:";
+	cin >> list[n].dtb;
+	return 1;
+}
+
+void XuatDSSV(student list[], int n)
+{
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Sinh vien " << i + 1 << endl;
+		cout << "|MSSV     |: " << list[i].MSSV << endl;;
+		cout << "|Ho Ten   |: " << list[i].hoten;
+		if (list[i].gioitinh == 1)
+			cout << "|Gioi Tinh|: Nu"  << endl;
+		else
+			cout << "|Gioi Tinh|: Nam" << endl;
+		cout << "|Dia Chi  |: " << list[i].diachi;
+		cout << "|Diem TB  |: " << list[i].dtb << endl;
+	}
+	system("pause");
+}
+
+int TimSV(student list[], int n, char x[])
+{
+	for (int i = 0; i < n; i++)
+		if (strcmp(list[i].MSSV, x) == 0)
+			return i; //Tìm thay
+	return -1; //Không tìm thay
+}
+
+void XoaSV(student list[], int& n, char x[])
+{
+	int vt = TimSV(list, n, x); //Tìm vi tri
+	if (vt == -1)
+		cout << "Khong tim thay Sinh Vien.";
+	else
+	{
+		for (int i = vt; i <= n - 2; i++)
+			list[i] = list[i + 1];
+		cout << "Da xoa sinh vien co MSSV: " << x << endl;
+		system("pause");
+		n--; //Giam so luong phan tu
+	}
+}
+void SapTheoDTB(student list[], int n)
+{
+	student tmp;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (list[i].dtb > list[j].dtb)
+			{
+				tmp = list[i];
+				list[i] = list[j];
+				list[j] = tmp;
+			}
+		}
+	}
+}
+int partition(student list[], int low, int high)
+{
+	float pivot = list[high].dtb;    
+	int left = low;
+	int right = high - 1;
+	while (true) {
+		while (left <= right && list[left].dtb < pivot) left++;
+		while (right >= left && list[right].dtb > pivot) right--;
+		if (left >= right) break;
+		swap(list[left], list[right]);
+		left++;
+		right--;
+	}
+	swap(list[left], list[high]);
+	return left;
+}
+void SapTheoHoTen(student list[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(list, low, high);
+		SapTheoHoTen(list, low, pi - 1);
+		SapTheoHoTen(list, pi + 1, high);
+	}
+}
+int main1()
+{
+	student stu[MAX];
+	int n = 0, s;
+	char x[10];
+
+	while (1)
+	{
+		system("cls");
+		cout << "\n\n================Menu================" << endl;
+		cout << "1. Nhap du lieu sinh vien." << endl;
+		cout << "2. Xuat danh sach sinh vien." << endl;
+		cout << "3. Xoa sinh vien" << endl;
+		cout << "4. Sap xep theo ÐTB." << endl;
+		cout << "5. Sap xep theo Ho va ten." << endl;
+		cout << "0. Ket thuc." << endl;
+		cout << "====================================" << endl;
+		cout << "Nhap lua chon(0-5):";
+		cin >> s;
+		if (s < 0 || s>5)
+		{
+			cout << "Lua chon khong hop le" << endl;
+		}
+		else if (s == 0)
+		{
+			break;
+		}
+		else if (s == 1)
+		{
+			system("cls");
+			Nhap1SV(stu, n);
+			n++;
+		}
+		else if (s == 2)
+		{
+			system("cls");
+			XuatDSSV(stu, n);
+		}
+		else if (s == 3)
+		{
+			std::cin.ignore(32767, '\n');
+			cout << "Nhap MSSV muon xoa: ";
+			cin >> x;
+			XoaSV(stu, n, x);
+		}
+		else if (s == 4)
+		{
+			SapTheoDTB(stu, n);
+			XuatDSSV(stu, n);
+		}
+		else if (s == 5)
+		{
+			SapTheoHoTen(stu,0, n-1);
+			XuatDSSV(stu, n);
+		}
+	}
+
+	return 0;
+}
